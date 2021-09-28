069f07bf973877d3b8b46da3d260089e783f64f0
VitorCunhaP <69974844+VitorCunhaP@users.noreply.github.com>
2021-09-28 13:37:29 -0300

    concluded

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import produce from "immer";
+import "../styles/tasklist.scss";
+
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
+import { valueToNode } from "@babel/types";
+
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
+export function TaskList() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  function handleCreateNewTask() {
+    if (newTaskTitle.trim() !== "") {
+      setTasks([
+        ...tasks,
+        {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false,
+        },
+      ]);
+    }
+    setNewTaskTitle("");
+  }
+
+  function handleToggleTaskCompletion(id: number) {
+    tasks.forEach((value, index) => {
+      if (value.id === id) {
+        setTasks(
+          produce(tasks, (draft) => {
+            draft[index].isComplete = !draft[index].isComplete;
+          })
+        );
+      }
+    });
+  }
+
+  function handleRemoveTask(id: number) {
+    tasks.forEach((value, index) => {
+      if (value.id === id) {
+        setTasks(
+          produce(tasks, (draft) => {
+            draft.splice(index, 1);
+          })
+        );
+      }
+    });
+  }
+
+  return (
+    <section className="task-list container">
+      <header>
+        <h2>Minhas tasks</h2>
+
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
+            onChange={(e) => setNewTaskTitle(e.target.value)}
+            value={newTaskTitle}
+          />
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}
+          >
+            <FiCheckSquare size={16} color="#fff" />
+          </button>
+        </div>
+      </header>
+
+      <main>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    readOnly
+                    checked={task.isComplete}
+                    onClick={() => handleToggleTaskCompletion(task.id)}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+                <p>{task.title}</p>
+              </div>
+
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
+                <FiTrash size={16} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </section>
+  );
+}
